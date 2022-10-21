7df9af241cff1a1d30558c8a9054024d1a3b6590
zarifrasul10 <109752125+zarifrasul10@users.noreply.github.com>
2022-10-21 15:49:30 -0600

    Update R script_201022.R

--- R script_201022.R
+++ R script_201022.R
@@ -1,23 +1,25 @@
 rm(list=ls())
-options(scipen = 100)
+options(scipen = 100)   ## Turning scientific notation off so that the final regression output does not contain scientific notation
 
 
 
+# reading in datasets 
 
-GDP_data <- read.csv("inputs/gdp_52-67.csv")
+GDP_data <- read.csv("inputs/gdp_52-67.csv")            
 LE_data <- read.csv("inputs/le_52-67.csv")
 
 
+ #merging datasets
 total <- merge(GDP_data,LE_data,by=c("continent","country","year"))
 
 
-
+#calculating GDP per capita
 gdpPercap = total$gdp/total$pop
 
-
+#putting gdpPercap in the table with the rest of the data
 total$gdpPercap <- gdpPercap
 
-
+#plotting 
 plot(subset(total, country =="Canada",
             select=c(year,lifeExp)), type = "l", main = "Life expectancy in Canada" )
 
@@ -25,7 +27,7 @@ plot(subset(total, country =="Canada",
 plot(subset(total, country =="Canada",
             select=c(year,gdpPercap)), type = "l", main = "Per Capita GDP in Canada" )
 
-
+#regressing life expectancy on per capita GDP
 
 reg = lm(formula = total$lifeExp~total$gdpPercap)
 summary(reg)
